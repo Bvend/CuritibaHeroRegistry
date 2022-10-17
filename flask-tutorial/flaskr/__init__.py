347ae1c8d85de4a443d137efe199efeec3cec144
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2022-10-17 11:56:26 -0300

    Tutorial até tmplates

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -28,11 +28,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
     return app
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
