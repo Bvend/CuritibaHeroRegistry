5d65f0335c574820b8516eb958bf0824165e34c6
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2022-10-17 23:21:24 -0300

    Functinoal social media (blogBlueprint)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CuritibaHeroRegistry
 Heroes social network web app developped for academic subject Systems Analysis and Development
+
+# To run
+flask --app flaskr --debug run
--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -30,6 +30,11 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     
     return app
 
